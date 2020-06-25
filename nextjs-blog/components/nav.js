23795f343bed9b16fe4206d4fe51de8c5bf08f2d
Angel Torres <angeltorres.dev@gmail.com>
2020-06-25 15:51:15 -0500

    added toggle collapse functionality to nav

--- nextjs-blog/components/nav.js
+++ nextjs-blog/components/nav.js
@@ -1,26 +1,33 @@
+import { useState } from 'react'
 import Link from 'next/link'
 
 export default function Nav() {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const handleToggle = (e) => {
+        console.log("is collapsed - ", isCollapsed)
+        if(isCollapsed) {
+            setIsCollapsed(false)
+        } else {
+            setIsCollapsed(true)
+        }
+    }
+
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <Link href="/"><a class="navbar-brand" href="#">AT</a></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" onClick={handleToggle}></span>
             </button>
-
-            <div class="collapse navbar-collapse" id="navbarColor02">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
+            <div className={`${isCollapsed ? "collapse navbar-collapse" : "navbar-collapse"}`} id="navbarColor02" right>
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
                         <Link href="/"><a class="nav-link" href="#">Blog <span class="sr-only">(current)</span></a></Link>
                     </li>
-                    <li class="nav-item active">
+                    <li className="nav-item active">
                         <Link href="/porfolio"><a class="nav-link" href="#">Porfolio <span class="sr-only">(current)</span></a></Link>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search" />
-                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>
     )
