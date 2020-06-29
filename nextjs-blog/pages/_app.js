e4c014e19db1604d959bdc2532f129a9325a162c
angel-torres <alas4224@gmail.com>
2020-06-28 23:44:05 -0500

    changed the navbar title and font

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,6 +1,17 @@
-import React from 'react'
-import '../styles/global.scss'
+import React from "react";
+import Head from "next/head";
+import "../styles/global.scss";
 
-export default function App({ Component, pageProps}) {
-    return <Component {...pageProps}/>
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Boogaloo&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
